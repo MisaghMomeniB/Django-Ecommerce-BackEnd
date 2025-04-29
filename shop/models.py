8b61d66a90815edc5ff9b094911eb976e75c2cfc
Misagh Momeni Bashusqeh <166559069+MisaghMomeniB@users.noreply.github.com>
2025-04-29 22:28:26 +0330

    create orderitem class for items

--- shop/models.py
+++ shop/models.py
@@ -50,3 +50,14 @@ class Order(models.Model):
 
     def total_price(self):
         return sum(item.total_price() for item in self.items.all())
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
+
+    def total_price(self):
+        return self.product.price * self.quantity
