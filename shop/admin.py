b0afad4f22a7e62ce33c7ebc8e00fad100b062a7
Misagh Momeni Bashusqeh <166559069+MisaghMomeniB@users.noreply.github.com>
2025-04-29 21:03:45 +0330

    create productadmin class for admin-register

--- shop/admin.py
+++ shop/admin.py
@@ -7,3 +7,9 @@ from .models import Category, Product
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
     prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'available', 'created']
+    list_filter = ['available', 'created', 'category']
+    prepopulated_fields = {'slug': ('name',)}
