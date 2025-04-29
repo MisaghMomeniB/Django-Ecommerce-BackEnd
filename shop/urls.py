769325068dfc5d705a45db443130c5e0072555fb
Misagh Momeni Bashusqeh <166559069+MisaghMomeniB@users.noreply.github.com>
2025-04-29 22:30:44 +0330

    set path

--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import ProtectedTestView
 from .views import CartView
+from .views import CreateOrderView, UserOrdersListView
 
 urlpatterns = [
     path('protected/', ProtectedTestView.as_view(), name='protected'),
     path('cart/', CartView.as_view(), name='cart'),
+    path('checkout/', CreateOrderView.as_view(), name='checkout'),
+    path('orders/', UserOrdersListView.as_view(), name='user-orders'),
 ]
