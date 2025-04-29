4fead69ab380017f1103e35ca0a5f99a7e1deb02
Misagh Momeni Bashusqeh <166559069+MisaghMomeniB@users.noreply.github.com>
2025-04-29 21:01:44 +0330

    create categoryadmin class for admin-register

--- shop/admin.py
+++ shop/admin.py
@@ -3,3 +3,7 @@ from .models import Category, Product
 
 # Register your models here.
 
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
