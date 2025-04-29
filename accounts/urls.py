61d542320e4e4075d93a930a918da8d9bb82a3f0
Misagh Momeni Bashusqeh <166559069+MisaghMomeniB@users.noreply.github.com>
2025-04-30 00:42:22 +0330

    update

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,5 +11,6 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name=('token_obtain_pair')),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh') 
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
