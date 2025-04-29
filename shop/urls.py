98b70252ddda6c0f1f3bf8f6d9c823bbd594ba3b
Misagh Momeni Bashusqeh <166559069+MisaghMomeniB@users.noreply.github.com>
2025-04-29 22:18:12 +0330

    add path

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import ProtectedTestView
+from .views import CartView
 
 urlpatterns = [
     path('protected/', ProtectedTestView.as_view(), name='protected'),
+    path('cart/', CartView.as_view(), name='cart'),
 ]
