914cf806134d7bfc7b7f92954d0f54751ca2188a
Misagh Momeni Bashusqeh <166559069+MisaghMomeniB@users.noreply.github.com>
2025-04-29 22:14:18 +0330

    create card class

--- shop/models.py
+++ shop/models.py
@@ -23,3 +23,11 @@ class Product(models.Model):
     def __str__(self):
         return self.name
     
+User = settings.AUT_USER_MODEL
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='cart')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Cart of {self.user.username}"
