f1d9fdd3a2fd8864c08ca5cd7eb50898b7f3f29e
Misagh Momeni Bashusqeh <166559069+MisaghMomeniB@users.noreply.github.com>
2025-04-29 21:46:18 +0330

    import librarys

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import RegisterSerializer
+from django.contrib.auth.models import User
 
 # Create your views here.
