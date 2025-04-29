619c665b71911018c705b1a4dc5e19fd50e3fa4a
Misagh Momeni Bashusqeh <166559069+MisaghMomeniB@users.noreply.github.com>
2025-04-29 22:48:55 +0330

    update path

--- shop/urls.py
+++ shop/urls.py
@@ -2,8 +2,14 @@ from django.urls import path
 from .views import ProtectedTestView
 from .views import CartView
 from .views import CreateOrderView, UserOrdersListView, PayOrderView
+from .views import product_list, product_detail, add_to_cart, view_cart, checkout
 
 urlpatterns = [
+    path('', product_list, name='product_list'),
+    path('product/<int:product_id>/', product_detail, name='product_detail'),
+    path('add-to-cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
+    path('cart/', view_cart, name='cart'),
+    path('checkout/', checkout, name='checkout'),
     path('protected/', ProtectedTestView.as_view(), name='protected'),
     path('cart/', CartView.as_view(), name='cart'),
     path('checkout/', CreateOrderView.as_view(), name='checkout'),
