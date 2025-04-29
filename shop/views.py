2643189e85e6a539aea4665365be33e5abdbaa70
Misagh Momeni Bashusqeh <166559069+MisaghMomeniB@users.noreply.github.com>
2025-04-29 22:17:45 +0330

    cart view class

--- shop/views.py
+++ shop/views.py
@@ -15,3 +15,42 @@ class ProtectedTestView(APIView):
 
     def get(self, request):
         return Response({"message": f"Hi {request.user.username}, Done!"})
+    
+class CartView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        cart, created = Cart.objects.get_or_create(user=request.user)
+        serializer = CartSerializer(cart)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AddToCartSerializer(data=request.data)
+        if serializer.is_valid():
+            product_id = serializer.validated_data['product_id']
+            quantity = serializer.validated_data['quantity']
+
+            cart, created = Cart.objects.get_or_create(user=request.user)
+            product = Product.objects.get(id=product_id)
+
+            item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+            if not created:
+                item.quantity += quantity
+            else:
+                item.quantity = quantity
+            item.save()
+
+            return Response({"message": "محصول به سبد اضافه شد."}, status=201)
+        return Response(serializer.errors, status=400)
+
+    def delete(self, request):
+        product_id = request.data.get('product_id')
+        cart = Cart.objects.filter(user=request.user).first()
+        if not cart:
+            return Response({"error": "سبدی وجود ندارد"}, status=404)
+
+        item = cart.items.filter(product_id=product_id).first()
+        if item:
+            item.delete()
+            return Response({"message": "محصول از سبد حذف شد."})
+        return Response({"error": "محصول در سبد پیدا نشد."}, status=404)
