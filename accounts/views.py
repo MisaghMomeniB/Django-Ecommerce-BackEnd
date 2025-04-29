beb580f75c2ee12958977762375875f689e44f1d
Misagh Momeni Bashusqeh <166559069+MisaghMomeniB@users.noreply.github.com>
2025-04-29 21:47:22 +0330

    create registerview class

--- accounts/views.py
+++ accounts/views.py
@@ -4,3 +4,7 @@ from .serializers import RegisterSerializer
 from django.contrib.auth.models import User
 
 # Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
